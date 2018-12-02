6c12f3f86a9052a46320c3dc314a82513f4a915a
rayman <raymanovg@gmail.com>
2018-12-02 20:20:31 +0300

    + Refactoring regex parser

--- src/RegexParser.php
+++ src/RegexParser.php
@@ -7,19 +7,21 @@ namespace App;
  */
 class RegexParser implements LogParser
 {
-    const REQUEST_KEY = 'request';
+    const PATH_KEY = 'path';
     const STATUS_KEY = 'status';
+    const SIZE_KEY = 'size';
     const URL_KEY = 'url';
     const USER_AGENT_KEY = 'user_agent';
 
-    private $pattern = '/^.*"(?<%s>.*HTTP\/[\d.]{3}).*"\s?(?<%s>[\s\d-]*)\s+"(?<%s>[\S]*)"\s?"(?<%s>.*)"$/';
+    private $pattern = '/^.+"[A-Z]+\s(?<%s>\/[\S]+)\s.+"\s+(?<%s>\d+)[\s-]+(?<%s>\d+).+"(?<%s>\S+)"\s+"(?<%s>.+)"$/';
 
     public function __construct()
     {
         $this->pattern = sprintf(
             $this->pattern,
-            self::REQUEST_KEY,
+            self::PATH_KEY,
             self::STATUS_KEY,
+            self::SIZE_KEY,
             self::URL_KEY,
             self::USER_AGENT_KEY
         );
@@ -34,27 +36,14 @@ class RegexParser implements LogParser
         return $this->build($matches);
     }
 
-    protected function getRequestPath(string $request)
-    {
-        if (!preg_match('/(?<path>\/\w+.*)\s+/', $request, $matches)) {
-            throw new \RuntimeException('Cannot retrieve request path');
-        }
-
-        return $matches['path'];
-    }
-
     protected function build($matches) : array
     {
-        $path = $this->getRequestPath($matches[self::REQUEST_KEY]);
-        $statusInfo = str_replace('-', '', $matches[self::STATUS_KEY]);
-        [$statusCode, $requestSize] = explode(' ', $statusInfo);
-
         return [
-            'status_code' => (int) $statusCode,
-            'request_path' => $path,
-            'request_size' => (int) $requestSize,
-            'url' => $matches[self::URL_KEY],
-            'user_agent' => $matches[self::USER_AGENT_KEY]
+            self::STATUS_KEY => (int) $matches[self::STATUS_KEY],
+            self::PATH_KEY => $matches[self::PATH_KEY],
+            self::SIZE_KEY => (int) $matches[self::SIZE_KEY],
+            self::URL_KEY => $matches[self::URL_KEY],
+            self::USER_AGENT_KEY => $matches[self::USER_AGENT_KEY]
         ];
     }
 }
